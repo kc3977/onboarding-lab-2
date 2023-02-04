87cd26440430844e7bdf58eb4ea848d5ef59dbfa
kc3977 <102318279+kc3977@users.noreply.github.com>
2023-02-04 12:18:31 -0500

    Update Exercise3.sv

--- rtl/Exercise3.sv
+++ rtl/Exercise3.sv
@@ -6,5 +6,10 @@ module Exercise3 (
     input [15:0] c,
     output [15:0] out
 );
+    wire a_in;
+    wire b_in;
+    Mystery1(a[1:0],b[7:0],c[7:0], a_in);
+    Mystery1(a[3:2],b[15:8],c[15:8], b_in);
+    Mystery2(a_in, b_in, clk, nReset, out);
 
 endmodule
